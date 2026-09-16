197339afd03572bb0a4a9800a4f88e0e60530bf7
elamed2007 <elamed2007@users.noreply.github.com>
2026-09-16 01:02:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hangman
+
+## Live Demo
+Visit Website: https://elamed2007.github.io/hangman-game/
+
+## Technologies
+- HTML5
+- CSS3
+- JavaScript
---
+++ script.js
@@ -0,0 +1,144 @@
+let categoryDisplay = document.getElementById("categorie")
+let wordDisplay = document.querySelector(".affichage")
+let N_attempts = document.getElementById("N_attempts")
+let style = document.getElementById("style")
+let btn_start = document.getElementById("btn_start")
+let info_game = document.getElementById("info_game")
+let gameBox = document.getElementById("game")
+let hello = document.getElementById("hello")
+let word = ""
+let wordLength = ""
+
+btn_start.onclick = function(){
+   info_game.style.display = "none"
+   gameBox.style.filter = "blur(0)" 
+}
+
+
+
+if(localStorage.user == "visited"){
+    gameBox.style.filter = "blur(0)"
+    info_game.style.display ="none"
+    hello.style.display ="none"
+}
+
+localStorage.user = ""
+
+if(localStorage.user != "visited"){
+   localStorage.user = "visited"
+}
+
+
+ async function getData(){
+    let pro = await fetch("words.json")
+    let data = await pro.json()
+    
+    // choose a random category index from words.json
+    let categoryIndex = Math.ceil(Math.random()*9)
+    // choose a random word index from words.json
+    let wordIndex = Math.ceil(Math.random()*9)
+    // choose the category using categoryIndex
+    let categories = data.categories[categoryIndex].nom
+    // choose the word using wordIndex and categoryIndex without spaces and uppercased
+    word = data.categories[categoryIndex].sub[wordIndex].toUpperCase().replace(/ /g,'')
+
+    create(word,categories)
+}
+
+
+ 
+// function to create boxes where we will put the letters of the word that the player has to find and the randomly selected category
+function create(word,categories){
+
+    // display the category on the page
+    categoryDisplay.innerHTML =`Category: ${categories}`
+   // word length without spaces
+   wordLength = word.length
+
+   // create the word display boxes
+   for(i=0;i < wordLength ;i++){
+     wordDisplay.innerHTML += `
+         <div class="box" id="box${i}"></div>
+     `
+   }
+   
+
+  }
+
+   getData()
+
+count = 0
+// the function to check if a letter is present in the word
+function checkLetter(letter){
+
+// if the letter is present in the word
+   if(word.includes(letter)){
+      // find the index of the letter in the word
+      letterIndex = word.indexOf(letter) 
+      let box = document.getElementById(`box${letterIndex}`)
+      // show the letter in the box based on letterIndex
+      box.innerHTML = `${letter}`
+      box.style.backgroundColor = 'green';
+      // replace the letter with a space so each letter is only used once
+      word = word.replace(letter," ")
+      let box_end = document.getElementById("box_end")
+      let emoji = document.getElementById("emoji")
+      let message_courage = document.getElementById("message_courage")
+      
+      if(word == " "*word_size){
+         box_end.style.display = "flex"
+         emoji.innerHTML = "&#128512;"
+         message_courage.innerHTML = "كمل، راك عْجبْنا الحمد لله  &#127881;"
+      }
+   // the letter is not present in the word
+   }else{
+      count += 1
+      N_attempts.innerHTML = `incorrect guesses: ${count}/7`
+      style.innerHTML += `button[value = "${letter}"]{ background: #D2042D;color:#000}
+                          button[value = "${letter}"]:hover{background-color: #D2042D;}`
+      displayHangman(count)
+         
+      if(count >= 7){
+         count = count
+         box_end.style.display = "flex"
+         emoji.innerHTML = "&#128512;"
+         message_courage.innerHTML = "ختّي/خويا تبارك الله عليك، درتي مجهودك. إن شاء الله فالمرجاية تربح. &#x2764;"
+      }
+
+      
+     
+   }  
+   
+   
+}
+
+// display a portion of the hangman image each time the player makes a mistake until the image is completed
+function displayHangman(count){
+   
+   switch(count){
+      case count:
+      let status = document.getElementById("Hangman")
+      status.innerHTML = `<img id="img_hangman" src="hangman photos/status${count}.png" alt="no-image">`;
+   }
+    
+}
+
+let btn_replay = document.getElementById("btn_replay")
+let btn_exit = document.getElementById("btn_exit")
+
+
+btn_replay.onclick = function(){
+    location.reload()
+    
+}
+
+btn_exit.onclick = function(){
+   window.close()
+}
+
+
+   
+
+   
+   
+
